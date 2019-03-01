b361a3eeb48e2e45d533e1cb49f49bfc4a1fdc93
Kevin Coughlin <keco@microsoft.com>
2019-03-01 13:01:45 -0800

    Scaffold SPFx FileBrowserWebPart

---
+++ src/webparts/fileBrowserWebPart/components/IFileBrowserWebPartProps.ts
@@ -0,0 +1,3 @@
+export interface IFileBrowserWebPartProps {
+  description: string;
+}
---
+++ src/webparts/fileBrowserWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IFileBrowserWebPartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'FileBrowserWebPartWebPartStrings' {
+  const strings: IFileBrowserWebPartWebPartStrings;
+  export = strings;
+}
