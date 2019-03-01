41164cd2c55269becb15ea351a0c11a149f73028
Kevin Coughlin <keco@microsoft.com>
2019-03-01 13:11:21 -0800

    Use Graph Base URL const

--- src/webparts/fileBrowserWebPart/components/FileBrowserWebPart.tsx
+++ src/webparts/fileBrowserWebPart/components/FileBrowserWebPart.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { IFileBrowserWebPartProps } from './IFileBrowserWebPartProps';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { GRAPH_BASE_URL } from '@microsoft/microsoft-graph-client';
 
 export default class FileBrowserWebPart extends BaseClientSideWebPart<IFileBrowserWebPartProps> {
   private getAuthToken(): Promise<string> {
     return this.context.aadTokenProviderFactory.getTokenProvider().then(tokenProvider => {
-      return tokenProvider.getToken('https://graph.microsoft.com/');
+      return tokenProvider.getToken(GRAPH_BASE_URL);
     });
   }
 
