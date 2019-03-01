42622ed6991a9313ed9662b6b19070734318e2f0
Kevin Coughlin <keco@microsoft.com>
2019-03-01 13:10:38 -0800

    Extend BaseClientSideWebPart and write getAuthToken private method

--- src/webparts/fileBrowserWebPart/components/FileBrowserWebPart.tsx
+++ src/webparts/fileBrowserWebPart/components/FileBrowserWebPart.tsx
@@ -1,25 +1,17 @@
 import * as React from 'react';
-import styles from './FileBrowserWebPart.module.scss';
 import { IFileBrowserWebPartProps } from './IFileBrowserWebPartProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 
-export default class FileBrowserWebPart extends React.Component<IFileBrowserWebPartProps, {}> {
+export default class FileBrowserWebPart extends BaseClientSideWebPart<IFileBrowserWebPartProps> {
+  private getAuthToken(): Promise<string> {
+    return this.context.aadTokenProviderFactory.getTokenProvider().then(tokenProvider => {
+      return tokenProvider.getToken('https://graph.microsoft.com/');
+    });
+  }
+
   public render(): React.ReactElement<IFileBrowserWebPartProps> {
     return (
-      <div className={ styles.fileBrowserWebPart }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div></div>
     );
   }
 }
--- src/webparts/fileBrowserWebPart/components/IFileBrowserWebPartProps.ts
+++ src/webparts/fileBrowserWebPart/components/IFileBrowserWebPartProps.ts
@@ -1,3 +1,3 @@
 export interface IFileBrowserWebPartProps {
-  description: string;
+
 }
